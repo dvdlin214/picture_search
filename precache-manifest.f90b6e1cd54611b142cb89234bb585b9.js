d47fe4573bfdc4e6b73549b0e5fe5827849469e3
Dave <dave.lin21@gmail.com>
2019-07-16 21:55:12 -0400

    Updates

---
+++ precache-manifest.f90b6e1cd54611b142cb89234bb585b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8739f63ac4bd25e0849",
+    "url": "/picture-search/static/css/main.ccfc76b8.chunk.css"
+  },
+  {
+    "revision": "e8739f63ac4bd25e0849",
+    "url": "/picture-search/static/js/main.e8739f63.chunk.js"
+  },
+  {
+    "revision": "02ba3405e7481ccc0a43",
+    "url": "/picture-search/static/js/1.02ba3405.chunk.js"
+  },
+  {
+    "revision": "ebbafb54dd5cd01e3d74",
+    "url": "/picture-search/static/js/runtime~main.ebbafb54.js"
+  },
+  {
+    "revision": "4fc8415b1ea7463791d41b8ef28dc9b6",
+    "url": "/picture-search/index.html"
+  }
+];
